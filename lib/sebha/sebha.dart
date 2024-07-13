5d9ce3fe3a78c9bdf3b3696cce0930c7507f46cb
Sherif Hamdy <sherif.hamdy.ahmed.93@gmail.com>
2024-07-13 14:29:44 +0300

    Finish sebha tab design.

--- lib/sebha/sebha.dart
+++ lib/sebha/sebha.dart
@@ -1,10 +1,61 @@
 import 'package:flutter/material.dart';
 
+import '../app_colors.dart';
+
 class SebhaScreen extends StatelessWidget {
   const SebhaScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.blue,);
+    return Container(
+      // color: Colors.red,
+      margin: EdgeInsets.symmetric(
+        horizontal: MediaQuery.of(context).size.width * 0.07,
+        vertical: MediaQuery.of(context).size.width * 0.02,
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Center(
+            child: Stack(alignment: Alignment.topCenter, children: [
+              Image.asset('assets/images/head_of_seb7a.png'),
+              Image.asset(
+                'assets/images/body_of_seb7a.png',
+                fit: BoxFit.cover,
+              )
+            ]),
+          ),
+          Text(
+            'عدد التسبيحات',
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.bodyMedium,
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text(
+              '30',
+              style: Theme.of(context).textTheme.bodyMedium,
+            ),
+            style: ElevatedButton.styleFrom(
+                backgroundColor: AppColors.primaryColor,
+                padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                )),
+          ),
+          Container(
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+                color: AppColors.primaryColor,
+                borderRadius: BorderRadius.circular(30)),
+            child: Text(
+              'سبحان الله',
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.bodyMedium,
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
